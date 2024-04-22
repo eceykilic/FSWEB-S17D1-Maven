81de5781d210cca034b1f538b29bba8fabae3fcb
eceykilic <eceykilic@gmail.com>
2024-04-22 17:05:30 +0300

    solutions

--- src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
+++ src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
@@ -2,6 +2,7 @@ package com.workintech.fswebs17d1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FswebS17d1Application {
@@ -9,5 +10,7 @@ public class FswebS17d1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(FswebS17d1Application.class, args);
 	}
+
+
 
 }
